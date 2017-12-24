dbc7e9c08e46ab490d87acd8999c05baac22c6cb
sombrastudios <nils.martel@concepts-system.com>
2017-12-24 19:10:29 +0100

    Add a test for is_redundant function and stuff like this

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,22 @@ use std::collections::HashMap;
 #[cfg(test)]
 mod tests {
     use super::*;
-    #[ignore]
+
     #[test]
     fn it_works() {
         let json_data: &str = "{\"name\":\"Nils\",\"age\":20,\"height\":1.83,\"programmer\":true}";
         let data = parse(json_data);
+    }
+
+    #[test]
+    fn test_redundant() {
+        let data: &str = "{\n  \"name\":\"Nils\",\n  \"age\":20,\n  \"height\":1.83,\n  \"programmer\":true\n}\n";
+        let mut result: String= String::new();
+        for c in data.chars().filter(|c| !is_redundant(c)) {
+            result.push(c);
+        }
+
+        assert_eq!(result.as_str(), "{\"name\":\"Nils\",\"age\":20,\"height\":1.83,\"programmer\":true}");
     }
 }
 
@@ -30,15 +41,16 @@ pub enum JSON {
 /// Parses JSON Data (in form of &str)
 /// @param data JSON Data(in form of String) that needs to be parsed
 pub fn parse(data: &str) -> JSON {
-    for c in iterator.filter(|c| !is_redundant(c)) {
-
+    for c in data.chars().filter(|c| !is_redundant(c)) {
     }
+
+    JSON::Null
 }
 
 /// returns true, if character is redundant for parsing JSON data
 /// @param c Character to be tested
-fn is_redundant(c: char) -> bool {
-    match c {
+fn is_redundant(c: &char) -> bool {
+    match *c {
         '\n' | '\t' | ' ' => return true,
         _ => return false
     }
